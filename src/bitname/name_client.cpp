7e5444b42ddb8d51e9036e75f6df4b5fb2ed5e44
Daniel Larimer <dlarimer@gmail.com>
2013-08-11 23:50:41 -0400

    hooked up delegates for bitname client

--- src/bitname/name_client.cpp
+++ src/bitname/name_client.cpp
@@ -1,15 +1,54 @@
 #include <bts/bitname/name_client.hpp>
 #include <bts/bitname/name_channel.hpp>
+#include <bts/bitname/name_miner.hpp>
 #include <fc/crypto/hex.hpp>
+#include <fc/filesystem.hpp>
+#include <bts/db/level_map.hpp>
+
+#include <fc/thread/thread.hpp>
 
 namespace bts { namespace bitname {
   
   namespace detail 
   {
-     class client_impl
+     class client_impl : public name_miner_delegate, public name_channel_delegate
      {
         public:
-          name_channel_ptr chan;
+
+          virtual void found_name_block( const name_block& new_block )
+          {
+             // make sure new_block is not stale... 
+             // check to see if it is good enough to solve the block, or just a trx
+             // broadcast it accordingly... 
+             // move on to the next name... 
+             _chan->submit_block( new_block );
+          }
+
+          /**
+           *   Called any time a new & valid name reg trx is received
+           */
+          virtual void pending_name_registration( const name_trx& pending_trx )
+          {
+              _miner.add_name_trx( pending_trx );
+          }
+          
+          /**
+           *   Called any time a new & valid name block is added to the
+           *   chain or replaces the head block.
+           */
+          virtual void name_block_added( const name_block& new_block )
+          {
+              _miner.set_prev( new_block.id() );
+
+              // TODO: for each name in pending-regs lookup how many
+              // confirmations it has, any with 6 confirmations can
+              // be removed from the pending state.
+          }
+
+          client::config                                   _config;
+          name_channel_ptr                                 _chan;
+          name_miner                                       _miner;
+          db::level_map<std::string,fc::ecc::public_key>   _pending_regs;
      };
 
   } // detail
@@ -17,14 +56,27 @@ namespace bts { namespace bitname {
   client::client( const peer::peer_channel_ptr& peer_ch )
   :my( new detail::client_impl() )
   {
-      my->chan = std::make_shared<bts::bitname::name_channel>(peer_ch);
+      my->_chan = std::make_shared<bts::bitname::name_channel>(peer_ch);
+      my->_chan->set_delegate( my.get() );
+      my->_miner.set_delegate( my.get() );
   }
   client::~client(){}
+
+  void client::configure( const client::config& client_config )
+  {
+     my->_config = client_config;
+     if( !fc::exists( my->_config.data_dir ) )
+     {
+       fc::create_directories( my->_config.data_dir / "bitname" );
+     }
+     my->_pending_regs.open( my->_config.data_dir / "bitname" / "pending", true /*create*/);
+     my->_miner.start( my->_config.max_mining_effort );
+  }
 
   name_record client::lookup_name( const std::string& name )
   {
      try {
-       name_header head = my->chan->lookup_name( name );
+       name_header head = my->_chan->lookup_name( name );
        name_record rec;
        rec.last_update  = head.utc_sec;
        rec.num_updates  = head.renewal;
@@ -53,12 +105,23 @@ namespace bts { namespace bitname {
      return fc::ecc::public_key( sig, digest );
   }
 
-  std::string client::register_name( const std::string& name, const fc::ecc::public_key& k )
+  void client::register_name( const std::string& bitname_id, const fc::ecc::public_key& name_key)
   {
-    FC_ASSERT( !"Not Implemented" );
+     my->_pending_regs.store( bitname_id, name_key );
   }
 
-  fc::ecc::compact_signature client::sign( const fc::sha256& digest, const std::string& name )
+  std::map<std::string,fc::ecc::public_key>  client::pending_name_registrations()const
+  {
+     std::map<std::string,fc::ecc::public_key> pending_regs;
+     // TODO: load from _pending_regs db
+     return pending_regs;
+  }
+  void                                       client::cancel_name_registration( const std::string& bitname_id )
+  {
+     my->_pending_regs.remove( bitname_id );
+  }
+
+  fc::ecc::compact_signature client::sign( const fc::sha256& digest, const std::string& bitname_id )
   {
     FC_ASSERT( !"Not Implemented" );
   }
