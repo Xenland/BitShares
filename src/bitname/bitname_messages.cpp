7a503da783fbe5151c8013aa2436e743ba77d803
Daniel Larimer <dlarimer@gmail.com>
2013-08-12 23:53:13 -0400

    removed message that wasn't needed

--- src/bitname/bitname_messages.cpp
+++ src/bitname/bitname_messages.cpp
@@ -5,7 +5,6 @@ namespace bts { namespace bitname {
 const message_type name_inv_message::type      = name_inv_msg;
 const message_type block_inv_message::type     = block_inv_msg;
 const message_type get_name_inv_message::type  = get_name_inv_msg;
-const message_type get_block_inv_message::type = get_block_inv_msg;
 const message_type get_headers_message::type   = get_headers_msg;
 const message_type get_block_message::type     = get_block_msg;
 const message_type get_name_message::type      = get_name_msg;
