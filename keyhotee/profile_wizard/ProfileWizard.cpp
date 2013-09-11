f8d5bf4ef0517fd566119c2f8daf7776b0be00aa
Xenland <xennetwork@hotmail.com>
2013-09-11 13:39:27 -0700

    Password validator(char,numbers,special chars check) and license/passport placeholders part2

--- keyhotee/profile_wizard/ProfileWizard.cpp
+++ keyhotee/profile_wizard/ProfileWizard.cpp
@@ -7,7 +7,6 @@
 
 #include <fc/log/logger.hpp>
 
-void display_main_window();
 class NymPage : public QWizardPage
 {
     public:
@@ -80,10 +79,15 @@ class ProfileEditPage : public QWizardPage
       ProfileEditPage( QWidget* parent )
       :QWizardPage(parent)
       {
+          //Init Validation Variables
+          noncharacters_present_list << "1" << "2" << "3" << "4" << "5" << "6" << "7" << "8" << "9" << "0" << "~" << "`" << "!" << "@" << "#" << "$" << "%" << "^" << "&" << "*" << "(" << ")" << "-" << "_" << "+" << "=" << "{" << "[" << "]" << "}" << "\\" << "|" << ";" << ":" << "'" << "\"" << ","  << "<" << ">" << "." << "/" << "?";
+
+
+          //Setup UI
           setTitle( tr( "Create your Keyhotee Profile" ) );
           ui.setupUi(this);
 
-
+          //Connections
           connect( ui.generaterandom, &QPushButton::clicked,
                    this, &ProfileEditPage::generateSeed );
           
@@ -119,22 +123,77 @@ class ProfileEditPage : public QWizardPage
 
       void loginPasswordEdited( const QString& password )
       {
+         //Track states
+         int local_password2_enabled = 1; //Default 1; Turns off when a warning/error in the inputted password occurs
+         QString password_warning_text = QString();
+
+
+         //Clear password input field 2
          ui.local_password2->setText( QString() );
-         if( password.size() < 8 )
-         {
-              ui.password_warning->setText( tr( "Password must be at least 8 characters" ) );
-              ui.local_password2->setEnabled(false);
+
+         //Password checking logic
+            int password_size = password.size();
+
+             /** Password Length Check **/
+             if( password_size < 8 )
+             {
+                 password_warning_text.append( tr( "Password must be at least 8 characters" ) );
+
+                 //Flag password two input disabled.
+                 local_password2_enabled = 0;
+             }
+
+             /** Entropy Check for Letters and numbers used) **/
+             //Loop through the entire string until satisfaction.
+             int characters_present = 0;
+             int noncharacters_present = 0;
+             for( int a = 0; a < password_size; a++ )
+             {
+                //Character or non-character?
+                 if(noncharacters_present_list.contains(password.at(a))){
+                    noncharacters_present = 1;
+                 }else{
+                     characters_present = 1;
+                 }
+             }
+
+             if(characters_present == 0 || noncharacters_present == 0){
+                 if(password_warning_text.size() > 0){
+                    password_warning_text.append( tr( " and must contain numbers (or symbols)" ) );
+                 }else{
+                    password_warning_text.append( tr( "Use characters and numbers (or symbols)" ) );
+                 }
+                 //Flag password two input disabled
+                 local_password2_enabled = 0;
+             }
+
+             /** Password1 & 2 Exact Match Check **/
+             if(local_password2_enabled == 1 && ui.local_password2->text() != ui.local_password1->text())
+             {
+                 if(password_warning_text.size() > 0){
+                     password_warning_text.append( tr( " and passwords do not match"));
+                 }else{
+                     password_warning_text.append( tr( "Passwords do not match" ) );
+                 }
+             }
+
+         //Disabled/Enable password 2 input field
+         if( local_password2_enabled == 1){
+             ui.local_password2->setEnabled(true);
+         }else if( local_password2_enabled == 0 ){
+             ui.local_password2->setEnabled(false);
          }
-         else
-         {
-              ui.password_warning->setText( tr( "Password do not match" ) );
-              ui.local_password2->setEnabled(true);
-         }
+
+         ui.password_warning->setText( password_warning_text );
+
          completeChanged();
       }
 
+
+
       void loginPasswordCheckEdited( const QString& password )
       {
+
          if( ui.local_password1->text() == password )
          {
             // TODO: check that password isn't on the commonly used
@@ -155,12 +214,14 @@ class ProfileEditPage : public QWizardPage
       }
 
       Ui::ProfileEditPage ui;
+
+    private:
+        QStringList noncharacters_present_list;
 };
 
 
 
-ProfileWizard::ProfileWizard( QWidget* parent )
-:QWizard(parent)
+ProfileWizard::ProfileWizard( QWidget* parent ) :QWizard(parent)
 {          
    setAttribute( Qt::WA_DeleteOnClose );
    setOption(HaveHelpButton, true);
@@ -210,24 +271,7 @@ void ProfileWizard::createProfile( int result )
 {
    if( _profile_edit->isComplete() )
    {
-      bts::profile_config conf;
-      conf.firstname   = _profile_edit->ui.first_name->text().toStdString();
-      conf.middlename  = _profile_edit->ui.middle_name->text().toStdString();
-      conf.lastname    = _profile_edit->ui.last_name->text().toStdString();
-      conf.brainkey    = _profile_edit->ui.brainkey->text().toStdString();
-
-      std::string password = _profile_edit->ui.local_password1->text().toStdString();
-
-      auto bapp = bts::application::instance();
       
-      auto pro = bapp->create_profile( conf, password );
-      assert( pro != nullptr );
-      
-      bts::identity new_ident;
-      new_ident.bit_id = _nym_page->_profile_nym_ui.keyhotee_id->text().toStdString();
-      pro->store_identity( new_ident );
-
-      display_main_window();
    }
 }
 
